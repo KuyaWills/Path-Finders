4483614c8ea3ff144ef59a24d310caa39920dba4
KuyaWills <kentvillahermosa@gmail.com>
2026-02-10 15:49:23 +0800

    fixing the issue with locale folder

--- src/app/[locale]/quiz/page.tsx
+++ src/app/[locale]/quiz/page.tsx
@@ -1,6 +1,10 @@
 import { setRequestLocale } from "next-intl/server";
 import { QuizFunnel } from "./QuizFunnel";
 
+// Quiz depends on client auth state and local storage,
+// so we mark it as dynamic to skip static prerendering at build time.
+export const dynamic = "force-dynamic";
+
 type Props = { params: Promise<{ locale: string }> };
 
 export default async function QuizPage({ params }: Props) {
